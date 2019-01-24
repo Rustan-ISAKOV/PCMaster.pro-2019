0d347933898b9f4cabba87cc65501cc03ba4e3bb
Rustan-ISAKOV <45059671+Rustan-ISAKOV@users.noreply.github.com>
2019-01-24 21:58:35 +0600

    Delete calculations.java

--- calculations.java
+++
@@ -1,9 +0,0 @@
-public class calculations {
-	public static void main(String[] arg) {
-		System.out.println("calculations");
-		int f1 = Integer.valueof(arg[0]);
-		int f2 = Integer.valueof(arg[1]);
-		int sum = f1 + f2;
-		System.out.println("calculations = " + sum);
-	}
-}
